e0bc26af61a7e9b4e6438ce125dec3a60eb107ca
Michael Mello <michaelkcms@gmail.com>
2024-04-24 14:42:43 -0300

    Finalização

---
+++ ContaBanco/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static class ContaTerminal {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite o número da conta:");
+            int numeroConta = scanner.nextInt();
+        
+            System.out.println("Digite a agência:");
+            String agencia = scanner.next();
+        
+            System.out.println("Digite o seu nome:");
+            String nome = scanner.next();
+        
+            System.out.println("Digite o seu saldo:");
+            double saldo = scanner.nextDouble();
+        
+            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        }
+    }
+}
